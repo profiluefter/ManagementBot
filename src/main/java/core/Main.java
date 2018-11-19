4162fa632513d154c7ba9c4d221e188ecd1883c2
DevClanDe <admin@devclan.de>
2018-11-19 17:36:18 +0100

    Initial commit

---
+++ src/main/java/core/Main.java
@@ -0,0 +1,42 @@
+package core;
+
+import listeners.readyListener;
+import listeners.shutdownListener;
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.OnlineStatus;
+
+import javax.security.auth.login.LoginException;
+
+public class Main {
+
+    public static JDABuilder b = new JDABuilder(AccountType.BOT);
+
+    public static void main(String[] args) {
+
+
+
+        b.setToken("NTE0MTA2NTI1MTA3NjE3ODE0.DtRu1A.2E7D8fe98Afqxxvl7ZWNvp8PFek");
+        b.setAutoReconnect(true);
+
+        b.setStatus(OnlineStatus.ONLINE);
+
+        addListeners();
+
+        try {
+            JDA jda = b.build();
+        } catch (LoginException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void addListeners() {
+
+        b.addEventListener(new readyListener());
+        b.addEventListener(new shutdownListener());
+
+    }
+
+}
---
+++ src/main/java/listeners/readyListener.java
@@ -0,0 +1,21 @@
+package listeners;
+
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.events.ReadyEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+public class readyListener extends ListenerAdapter {
+
+
+    public void onReady(ReadyEvent e) {
+
+        String startMSG = "HI :wave: ";
+
+        for(Guild g : e.getJDA().getGuilds()) {
+            g.getTextChannels().get(0).sendMessage(startMSG).queue();
+
+        }
+
+    }
+
+}
