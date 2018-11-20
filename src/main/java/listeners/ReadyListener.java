0663271ea0ec98d4a2ebe469fee896cfacde0ab8
DevClanDe <admin@devclan.de>
2018-11-20 17:50:17 +0100

    added Commands

--- src/main/java/listeners/ReadyListener.java
+++ src/main/java/listeners/ReadyListener.java
@@ -1,8 +1,12 @@
 package listeners;
 
+import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.events.ReadyEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
+import util.STATIC;
+
+import java.awt.*;
 
 public class ReadyListener extends ListenerAdapter {
 
@@ -12,7 +16,8 @@ public class ReadyListener extends ListenerAdapter {
         String startMSG = "HI :wave: ";
 
         for(Guild g : e.getJDA().getGuilds()) {
-            g.getTextChannels().get(0).sendMessage(startMSG).queue();
+
+            g.getTextChannels().get(0).sendMessage(STATIC.done.setDescription(startMSG).build()).queue();
 
         }
 
